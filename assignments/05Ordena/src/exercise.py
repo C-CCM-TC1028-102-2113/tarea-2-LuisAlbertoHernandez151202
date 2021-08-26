88a109a65801c33f0b9fc3a5216482b99a160ae7
LuisAlbertoHernandez151202 <89438566+LuisAlbertoHernandez151202@users.noreply.github.com>
2021-08-25 20:55:45 -0500

    Update exercise.py

--- assignments/05Ordena/src/exercise.py
+++ assignments/05Ordena/src/exercise.py
@@ -1,5 +1,15 @@
 def main():
     # Escribe el código adecuado para completar el programa
+    print("Buenas tardes, escriba 3 números")
+x=int(input("Ingresa el primer número:")
+y=int(input("Ingresa el segundo número:")
+z=int(input("ingresa el tercer número:")
+
+a= min(x,y,z)
+c=max(x,y,z)
+b=(x+y+z)-a-c
+print("Los números ordenados son:{},{} y {}".format(a,b,c))
+      
     pass
 
 
