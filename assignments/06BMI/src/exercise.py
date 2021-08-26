6cd04011f4791d5abb4913f47c0998b112b12369
LuisAlbertoHernandez151202 <89438566+LuisAlbertoHernandez151202@users.noreply.github.com>
2021-08-25 21:03:53 -0500

    Update exercise.py

--- assignments/06BMI/src/exercise.py
+++ assignments/06BMI/src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
+    print("Buenas tardes, ingrese por favor su peso y su altura")
+peso=int(input("Ingrese su peso en kg, por favor:")
+altura=int(input("Ingrese su altura en metros, por favor")
+IMC=peso/altura
+print("Su IMC:")
+if IMC > 20:
+print("Su IMC es bajo")
     pass
 if __name__=='__main__':
     main()
