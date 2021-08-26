8cfd6919e178dfbef06cc057d92eedde3659502c
LuisAlbertoHernandez151202 <89438566+LuisAlbertoHernandez151202@users.noreply.github.com>
2021-08-25 19:52:23 -0500

    Update exercise.py

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,6 +1,10 @@
 
 def main():
     #Escribe tu código debajo de esta línea
+    print("Buenas tardes, ingresa tu edad para ver si eres apto para obtener tu licencia")
+edad=int(input())
+if edad < 18:
+    print("Licencia denegada")
     pass
 
 
